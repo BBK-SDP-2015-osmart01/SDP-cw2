8fa3fa5266a6562d828f183b28114f9123a80750
Oliver Smart <osmart@smartsci.uk>
2015-03-16 16:56:04 +0000

    JUnit of Board.getPossibleMoves(Player) written. The initial stub method returns null so we get 4 failures, messages look good to me.
    Failure list:
    
    JUnitTestBoardGetPossibleMoves
    JUnitTestBoardGetPossibleMoves
    testEmptyBoard(JUnitTestBoardGetPossibleMoves)
    java.lang.AssertionError: 
    empty board
    | | | | | | | |
    | | | | | | | |
    | | | | | | | |
    | | | | | | | |
    | | | | | | | |
    | | | | | | | |
    So getPossibleMoves should return all 7 columns.
    Expected: is [<RED put a piece in column 0>, <RED put a piece in column
    1>, <RED put a piece in column 2>, <RED put a piece in column 3>, <RED
    put a piece in column 4>, <RED put a piece in column 5>, <RED put a
    piece in column 6>]
         but: was null
    	at ...
    	
    testBoardWhereRedHasWon(JUnitTestBoardGetPossibleMoves)
    java.lang.AssertionError: 
    Red has cheated and won
    | | | | | | | |
    | | | | | | | |
    | |R| | | | | |
    | |R| | | | | |
    | |R| | | | | |
    | |R| | | | | |
    Specification says getPossibleMoves should return empty array.
    Expected: is []
         but: was null
    	at ..
    	
    testFillEvenCols(JUnitTestBoardGetPossibleMoves)
    java.lang.AssertionError: 
    fill even columns
    |R| |R| |R| |R|
    |Y| |Y| |Y| |Y|
    |R| |R| |R| |R|
    |Y| |Y| |Y| |Y|
    |R| |R| |R| |R|
    |Y| |Y| |Y| |Y|
    So getPossibleMoves should return odd columns.
    Expected: is [<YELLOW put a piece in column 1>, <YELLOW put a piece in
    column 3>, <YELLOW put a piece in column 5>]
         but: was null
    	at ...
    	
    testDrawBoard(JUnitTestBoardGetPossibleMoves)
    java.lang.AssertionError: 
    Filled board with no winner.
    |Y|Y|R|R|Y|Y|R|
    |R|R|Y|Y|R|R|Y|
    |Y|Y|R|R|Y|Y|R|
    |R|R|Y|Y|R|R|Y|
    |Y|Y|R|R|Y|Y|R|
    |R|R|Y|Y|R|R|Y|
    Specification says getPossibleMoves should return empty array.
    Expected: is []
         but: was null
    	at ...

--- src-testing/JUnitTestBoardGetPossibleMoves.java
+++ src-testing/JUnitTestBoardGetPossibleMoves.java
@@ -6,8 +6,9 @@ import static org.hamcrest.CoreMatchers.is;
 /**
  * JUnit tests for {@link Board#getPossibleMoves(Player)}
  * <p>
- * Note that as the tested Class in default package JUnit tests have to be in
- * default too.
+ * Note that as the tested Class is in default package JUnit tests have to be in
+ * default as well! Split into separate source directory is all that can be
+ * done.
  * 
  * @author Oliver Smart (MSc CS) & Daryl Smith (MSc IT)
  */
@@ -16,7 +17,6 @@ public class JUnitTestBoardGetPossibleMoves {
 	@Test
 	public void testEmptyBoard() {
 		Board empty = new Board();
-		System.out.println("check empty board :\n" + empty + "\n");
 		Player red = Player.RED;
 		// get the moves
 		Move possibleMoves[] = empty.getPossibleMoves(red);
@@ -24,8 +24,8 @@ public class JUnitTestBoardGetPossibleMoves {
 		Move expectedMoves[] = new Move[] { new Move(red, 0), new Move(red, 1),
 				new Move(red, 2), new Move(red, 3), new Move(red, 4),
 				new Move(red, 5), new Move(red, 6) };
-		assertThat(
-				"\nall 7 columns should be available for move in empty board",
+		assertThat("\nempty board\n" + empty
+				+ "So getPossibleMoves should return all 7 columns.",
 				possibleMoves, is(expectedMoves));
 	}
 
@@ -38,23 +38,43 @@ public class JUnitTestBoardGetPossibleMoves {
 			if (cc % 2 == 0) // even
 				fillColumnAlternating(board, plr, cc);
 		}
-		// check that worked by printing it out
-		System.out.println("check board where even columns filled:\n" + board
-				+ "\n");
-
 		Move possibleMoves[] = board.getPossibleMoves(plr);
 		// should get back odd columns
 		Move expectedMoves[] = new Move[] { new Move(plr, 1), new Move(plr, 3),
 				new Move(plr, 5) };
-		assertThat("\nfilled even columns, odds should be available.",
+		assertThat("\nfill even columns\n" + board
+				+ "So getPossibleMoves should return odd columns.",
 				possibleMoves, is(expectedMoves));
 	}
 
 	/**
-	 * test a board where someone has won.
+	 * test a board where someone has already won. According to the method
+	 * specification:
+	 * <p>
+	 * If the board has a winner (four things of the same colour in a row), no
+	 * move is possible because the game is over. Specification says that
+	 * result should be empty array.
 	 */
-	
-	
+	@Test
+	public void testBoardWhereRedHasWon() {
+		Board board = new Board();
+		Player red = Player.RED;
+		// red has cheated and played four into column 1
+		board.makeMove(new Move(red, 1));
+		board.makeMove(new Move(red, 1));
+		board.makeMove(new Move(red, 1));
+		board.makeMove(new Move(red, 1));
+		System.out.println("check board where red has cheated and won:\n"
+				+ board + "\n");
+		Move possibleMoves[] = board.getPossibleMoves(red);
+		Move expectedMoves[] = new Move[0]; // spec says empty
+		assertThat(
+				"\nRed has cheated and won\n"
+						+ board
+						+ "Specification says getPossibleMoves should return empty array.",
+				possibleMoves, is(expectedMoves));
+	}
+
 	/**
 	 * fill a board with a non-winning pattern
 	 */
@@ -67,9 +87,11 @@ public class JUnitTestBoardGetPossibleMoves {
 		System.out.println("check board completely filled to a draw:\n" + board
 				+ "\n");
 		Move possibleMoves[] = board.getPossibleMoves(plr);
-		Move expectedMoves[] = new Move[0]; // empty array
+		Move expectedMoves[] = new Move[0]; // empty array it is full
 		assertThat(
-				"\nDrawn filled board: 'if all columns are full, return an array of length 0.'",
+				"\nFilled board with no winner.\n"
+						+ board
+						+ "Specification says getPossibleMoves should return empty array.",
 				possibleMoves, is(expectedMoves));
 	}
 
